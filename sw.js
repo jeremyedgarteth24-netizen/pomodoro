27953edc40666e4716dc8f20d1a2d58ee80c34f5
jeremyedgarteth24-netizen <jeremyedgarteth24@gmail.com>
2026-03-12 01:10:53 -0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'pomodoro-v1';
+const assets = ['./', './index.html', './manifest.json', './sw.js'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
